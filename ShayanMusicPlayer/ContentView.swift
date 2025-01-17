37ef0811e2627420aa4917513d0a7bb41167e068
Hamed Abasi <ha.abasi@gmail.com>
2025-01-17 09:00:06 +0330

    Initial Commit

---
+++ ShayanMusicPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShayanMusicPlayer
+//
+//  Created by Hamed Abasi on 10/28/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShayanMusicPlayer/ShayanMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShayanMusicPlayerApp.swift
+//  ShayanMusicPlayer
+//
+//  Created by Hamed Abasi on 10/28/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct ShayanMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
